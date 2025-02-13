b1f8c8d8617f02da2345aa83916499a4929588e5
Jack White <jackhwhite.83@gmail.com>
2025-02-13 12:41:04 -0700

    Implement functions for test ready

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+    if (number < 0) return `ERROR`;
+    let userString = ``;
+    for (let i = 0; i < number; i++) {
+        userString += string;
+    }
+    return userString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function (min, max) { // Create function expression
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return `ERROR`; // Check if the number is an integer
+    if (min < 0 || max < 0) return `ERROR`; // Check for negative numbers
+    if (min > max) {
+        const temp = min; // Switch min and max numbers
+        min = max;
+        max = temp;
+    }
+
+    let sum = 0; // Variable to hold final sum
+    for (let i = min; i <= max; i++) { // Loop through all the numbers
+        sum += i; // Add the loop numbers to the sum
+    }
+
+    return sum; // Return final sum
 
 };
 
